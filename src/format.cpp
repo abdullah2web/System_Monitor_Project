b52afacf919fae7bd85aff8dfd1c34ed79fb183a
Abdullah ALShayie <36479470+abdullah2web@users.noreply.github.com>
2021-02-16 11:06:11 +0300

    Add files via upload

--- src/format.cpp
+++ src/format.cpp
@@ -10,6 +10,31 @@ string Format::ElapsedTime(long seconds) {
     seconds = seconds % 3600;
     minutes = seconds / 60;
     seconds = seconds % 60;
-    return std::to_string(hours) + ":" + std::to_string(minutes) + ":" +
-        std::to_string(seconds); 
+
+    string hours_string;
+    string minutes_string;
+    string seconds_string;
+
+    if (hours < 10){
+        string hours_string = "0" + std::to_string(hours);
+    }
+    else {
+        string hours_string = std::to_string(hours);
+    }
+
+    if (minutes < 10){
+        string minutes_string = "0" + std::to_string(minutes);
+    }
+    else {
+        string minutes_string = std::to_string(minutes);
+    }
+
+    if (seconds < 10){
+        string seconds_string = "0" + std::to_string(seconds);
+    }
+    else {
+        string seconds_string = std::to_string(seconds);
+    }
+
+    return hours_string + ":" + minutes_string + ":" + seconds_string;
 }
